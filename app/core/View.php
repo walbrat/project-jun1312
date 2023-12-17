efe9e4a69b989e25a7f1c5df69a371e3f2d22b15
yourii.korchevskii <yourii.korchevskii@forforce.com>
2023-12-17 21:30:46 +0200

    Router working

--- app/core/View.php
+++ app/core/View.php
@@ -5,17 +5,32 @@ namespace core;
 class View
 {
     protected string $template = 'layout';
-
+    public $page;
+    const VIEW_DIR = VIEWS_FOLDER;
+    const TEMPLATE_DIR = TEMPLATES_FOLDER;
+    const PAGE_DIR = PAGES_FOLDER;
+    
     public function __construct($template = null)
     {
         if ($template !== null) {
             $this->template = $template;
         }
     }
-
+    
     public function render(string $page, array $data = [])
     {
+        $this->page = $page;
         extract($data);
-        include_once TEMPLATES_FOLDER . $this->template . '.php';
+        include_once $this->getTemplatePath();
+    }
+    
+    private function getPagePath()
+    {
+        return self::VIEW_DIR . self::PAGE_DIR . DIRECTORY_SEPARATOR . "{$this->page}.php";
+    }
+    
+    private function getTemplatePath()
+    {
+        return self::TEMPLATE_DIR . DIRECTORY_SEPARATOR . "{$this->template}.php";
     }
 }
