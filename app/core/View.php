2258a9d8f515f9a0dab6a1d83667dbbb65bd6a72
OpeR <pit-stop@3g.ua>
2023-12-18 13:14:58 +0200

    add new code

--- app/core/View.php
+++ app/core/View.php
@@ -5,11 +5,13 @@ namespace core;
 class View
 {
     protected string $template = 'layout';
+    protected string $admin_template = 'admin_layout';
 
-    public function __construct($template = null)
+    public function __construct($template = null, $admin_template = null)
     {
         if ($template !== null) {
             $this->template = $template;
+            $this->admin_template = $admin_template;
         }
     }
 
@@ -17,5 +19,11 @@ class View
     {
         extract($data);
         include_once TEMPLATES_FOLDER . $this->template . '.php';
+    }
+
+    public function adminRender(string $page, array $data = [])
+    {
+        extract($data);
+        include_once TEMPLATES_FOLDER . $this->admin_template . '.php';
     }
 }
