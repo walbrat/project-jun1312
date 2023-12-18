56ca45cb08a7f0162f442f6e882a4cd940cebb43
OpeR <pit-stop@3g.ua>
2023-12-18 13:16:22 +0200

    add new code

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,14 +1,16 @@
 <?php
+include_once '..'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.php';
 spl_autoload_register(function ($class) {
-    $path = '../app/' . $class . '.php';
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+    $path = '..'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.'' . $class . '.php';
     if (file_exists($path)) {
+        // TODO
+        //var_dump($path);
         include_once $path;
         return true;
     }
     return false;
 });
-
-include_once 'config/config.php';
-
-
 \core\Router::init();
+
+
