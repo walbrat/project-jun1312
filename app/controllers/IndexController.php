f0fea2c207ddf02c2d31dcb736a91abd4ca530b2
OpeR <pit-stop@3g.ua>
2023-12-18 13:14:19 +0200

    add new code

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -8,7 +8,10 @@ class IndexController extends BaseController
 {
     public function index()
     {
-        echo 'hello from IndexController';
-        $this->view->render('index');
+        $data = [];
+        $data['title'] = 'Головна сторінка';
+        $data['button_name'] = 'Головна';
+        $data['content'] = 'Тут буде текст з бази';
+        $this->view->render('index', $data);
     }
 }
