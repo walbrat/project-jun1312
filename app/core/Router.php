e14feff60f79d53afd9bc4004778fd76c424a12b
OpeR <pit-stop@3g.ua>
2023-12-18 13:14:45 +0200

    add new code

--- app/core/Router.php
+++ app/core/Router.php
@@ -5,24 +5,47 @@ namespace core;
 class Router
 {
     const DEFAULT_ACTION = 'index';
-    const DEFAULT_CONTROLLER = 'index';
+    const DEFAULT_CONTROLLER = 'Index';
+    const DEFAULT_ADMIN_CONTROLLER = 'Admin';
 
     /**
      * get action GET param and call same method in object of Main
      */
     static public function init()
     {
-        $actionInput = filter_input(INPUT_GET, 'action');
-        $action = $actionInput ?? self::DEFAULT_ACTION;
-        $action = trim($action);
-        $action = strtolower($action);
+        $route = $_SERVER['REQUEST_URI'];
+        // Разбиваем строку по символу "/"
+        $routeArray = explode('/', $route);
+        // Фильтруем пустые элементы массива
+        $routeArray = array_filter($routeArray);
+        // Преобразуем индексы массива в числа (если нужно)
+        $routeArray = array_values($routeArray);
 
-        $controllerInput = filter_input(INPUT_GET, 'controller');
-        $controllerName = $controllerInput ?? self::DEFAULT_CONTROLLER;
-        $controllerName = trim($controllerName);
-        $controllerName = strtolower($controllerName);
-        $controllerName = ucfirst($controllerName);
-        $controllerName = '\controllers\\'.$controllerName.'Controller';
+        if (isset($routeArray[0]) && $routeArray[0] == 'admin') {
+            $controllerName = $routeArray[1] ?? self::DEFAULT_ADMIN_CONTROLLER;
+            $controllerName = trim($controllerName);
+            $controllerName = strtolower($controllerName);
+            $controllerName = ucfirst($controllerName);
+            $controllerName = '\controllers\admin\\'.$controllerName.'Controller';
+
+            $action = $routeArray[2] ?? self::DEFAULT_ACTION;
+            $action = trim($action);
+            $action = strtolower($action);
+            $id = $routeArray[3] ?? null;
+
+        } else {
+            $controllerName = $routeArray[0] ?? self::DEFAULT_CONTROLLER;
+            $controllerName = trim($controllerName);
+            $controllerName = strtolower($controllerName);
+            $controllerName = ucfirst($controllerName);
+            $controllerName = '\controllers\\'.$controllerName.'Controller';
+
+            $action = $routeArray[1] ?? self::DEFAULT_ACTION;
+            $action = trim($action);
+            $action = strtolower($action);
+            $id = $routeArray[2] ?? null;
+        }
+
         if(!class_exists($controllerName)){
             self::notFound();
         }
@@ -30,7 +53,7 @@ class Router
         if (!method_exists($controller, $action)) {
             self::notFound();
         }
-        $controller->$action();
+        $controller->$action($id);
     }
 
     /**
