2b886c8de18f495b680e212bd8165767596b1de7
OpeR <pit-stop@3g.ua>
2023-12-23 19:20:22 +0200

    app/view/pages/includes/header.php change href a

--- app/view/pages/includes/header.php
+++ app/view/pages/includes/header.php
@@ -1,12 +1,7 @@
 <div class="w3-bar w3-black">
 
     <a class="w3-bar-item w3-button" href="/" class="header_name">Home</a>
-    <a class="w3-bar-item w3-button" href="/page/index" class="header_name">Pages</a>
-    <a class="w3-bar-item w3-button" href="/admin/" class="header_name">Users</a>
-
-
-    <a class="w3-bar-item w3-button w3-right" href="/auth/login" class="header_name">Login</a>
-    <a class="w3-bar-item w3-button w3-right" href="/auth/signup" class="header_name">Register</a>
-
+    <a class="w3-bar-item w3-button" href="/index/index?id=1" class="header_name">Pages</a>
+    <a class="w3-bar-item w3-button" href="/dashboard/" class="header_name">Users</a>
 
 </div>
