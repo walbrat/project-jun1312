3fa6bbf6efa5c25c29e6f646146e28b9d54db809
OpeR <pit-stop@3g.ua>
2023-12-23 18:53:56 +0200

    AdminController format and add new method checkRootUser()

--- app/controllers/admin/AdminController.php
+++ app/controllers/admin/AdminController.php
@@ -2,54 +2,52 @@
 
 namespace controllers\admin;
 
-use core\ResourceController;
+use core\BaseController;
 use core\Router;
+use core\View;
+use models\User;
 
-class AdminController extends ResourceController
+
+class AdminController extends BaseController
 {
-
-
     /**
-     * @inheritDoc
+     * @return void
      */
     public function index(): void
     {
+        $page = 'index';
         $this->data['title'] = 'Адмін панель';
-        if (file_exists('install.php')) {
-            Router::redirect('/install.php');
+        $this->checkRootUser();
+        $this->view->adminRender($page, $this->data);
+    }
+
+    /**
+     * Цей метод перевіряє існування файла install.php, та чи є в базі даних користувачі.
+     * @return mixed
+     */
+    public function checkRootUser(): mixed
+    {
+        $page = 'install';
+        $installFile = ADMIN_PAGES_FOLDER . $page . '.php';
+        $users = new User();
+        $allUsers = $users->getUsers();
+
+        if (file_exists($installFile) && empty($allUsers)) {
+            $url = Router::getUrl('install', 'index');
+            Router::redirect($url);
+        } else if (!empty($allUsers)) {
+            $installTemplateFile = TEMPLATES_ADMIN_FOLDER . $page . '_template.php';
+            $installControllerFile =  __DIR__ . DIRECTORY_SEPARATOR . ucfirst($page) . 'Controller.php';
+            if (file_exists($installFile)) {
+                unlink($installFile);
+            }
+            if (file_exists($installTemplateFile)) {
+                unlink($installTemplateFile);
+            }
+            if (file_exists($installControllerFile)) {
+                unlink($installControllerFile);
+            }
         }
-        $this->view->adminRender('index', $this->data);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function create(): void
-    {
-        // TODO: Implement create() method.
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function show(int $id): void
-    {
-        // TODO: Implement show() method.
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function update(int $id): void
-    {
-        // TODO: Implement update() method.
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function destroy(int $id): void
-    {
-        // TODO: Implement destroy() method.
+        return false;
     }
 }
