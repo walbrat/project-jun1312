fdc0e34520fe17262588cd4946404590cfaf9968
OpeR <pit-stop@3g.ua>
2023-12-23 19:00:51 +0200

    BaseModel add method escape(), add method getLastId()

--- app/core/BaseModel.php
+++ app/core/BaseModel.php
@@ -14,4 +14,23 @@ class BaseModel
     {
         $this->db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     }
+
+    /**
+     * Екранує спеціальні символи у рядку для використання у SQL-вираженні, використовуючи поточний набір символів з'єднання
+     * @param $value
+     * @return string
+     */
+    public function escape($value)
+    {
+        return $this->db->real_escape_string($value);
+    }
+
+    /**
+     * повертае id останнього вставленого рядка у будьяку таблицю
+     * @return int|string
+     */
+    public function getLastId()
+    {
+        return $this->db->insert_id;
+    }
 }
