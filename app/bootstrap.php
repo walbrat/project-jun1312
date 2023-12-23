8594609fd0bea9dd8c1c479aafea996341a839a4
OpeR <pit-stop@3g.ua>
2023-12-23 19:25:56 +0200

    bootstrap.php add $class = str_replace('\\', DIRECTORY_SEPARATOR, $class); in method spl_autoload_register(), add try catch

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,15 +1,19 @@
 <?php
-include_once 'config'.DIRECTORY_SEPARATOR.'config.php';
-spl_autoload_register(function ($class) {
-    $path = '../app/' . $class . '.php';
-    if (file_exists($path)) {
-        // TODO
-//        var_dump($path, APP_DIR);
-        include_once $path;
-        return true;
+    include_once 'config'.DIRECTORY_SEPARATOR.'config.php';
+    spl_autoload_register(function ($class) {
+        $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+        $path = '..'.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.$class . '.php';
+
+        if (file_exists($path)) {
+            // TODO
+            //var_dump($path);
+            include_once $path;
+            return true;
+        }
+        return false;
+    });
+    try {
+        \core\Router::init();
+    }catch (\Exception $e) {
+        echo $e->getMessage();
     }
-    return false;
-});
-\core\Router::init();
-
-
