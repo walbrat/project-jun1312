58f6c52dac47d22fd1a5e93d5ab8d49623129e5d
OpeR <pit-stop@3g.ua>
2023-12-24 17:32:30 +0200

    admin header.php change logo img

--- app/view/admin/includes/header.php
+++ app/view/admin/includes/header.php
@@ -1,4 +1,5 @@
-<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:25%">
+<div class="w3-sidebar w3-light-grey w3-bar-block" style="width:15%">
+    <img class="logo" src="/images/logo.svg" alt="">
     <h3 class="w3-bar-item">Menu</h3>
     <a href="/dashboard/page" class="w3-bar-item w3-button">Pages</a>
     <a href="/dashboard/user" class="w3-bar-item w3-button">Users</a>
