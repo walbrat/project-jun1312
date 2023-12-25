774fc09ba665e73ee56fff6177cac29d590b4973
walbrat <walbrat@gmail.com>
2023-12-25 11:49:35 +0200

    $pages

--- app/view/pages/index.php
+++ app/view/pages/index.php
@@ -1,7 +1,7 @@
 
 
 <div class="w3-bar w3-black">
-    <?php foreach ($content as $page){?>
+    <?php foreach ($pages as $page){?>
     <a class="w3-bar-item w3-button" href="/index/index/<?php echo $page['id']; ?>"class="header_name" > <?php echo $page['title']?></a>
     <?php }?>
 </div>
